303dc3a53005af8fffdbd2c73e7ba46f1b197829
Barb, Denisa <denisa.barb@swoboda.com>
2026-09-21 11:43:40 +0300

    update popup UI

--- components/popup.tsx
+++ components/popup.tsx
@@ -90,71 +90,66 @@ const Popup: React.FC<PopupProps> = ({ isOpen, session, onClose, save, deleteSes
         <div className="fixed inset-0 flex items-center justify-center z-50 bg-black/50">
             <div className="w-96 bg-gray-800 rounded-lg shadow-lg p-6">
                 <h2 className="text-lg font-semibold text-white mb-4">Edit Session</h2>
-                <p className="text-gray-400 mb-2">
-                    <strong>Person ID: </strong>
-                    <input type="text" value={session?.person_id} readOnly />
-                </p>
-                <p className="text-gray-400 mb-2">
-                    <strong>Name: </strong>
-                    <input type="text" value={`${session?.person_first_name} ${session?.person_last_name}`} readOnly />
-                </p>
-                <p className="text-white mb-2">
-                    <strong>Line: </strong>
-                    {/* <input type="dropdown" readOnly={false} onChange={(e) => setLine(e.target.value)}  /> */}
 
+                <div className="grid grid-cols-[100px_1fr] items-center gap-3 text-gray-400">
+                    <p>
+                        <strong>Person ID: </strong>
+
+                    </p>
+                    <input type="text" value={session?.person_id} readOnly />
+                </div>
+
+                <div className="grid grid-cols-[100px_1fr] items-center gap-3 text-gray-400">
+                    <p>
+                        <strong>Name: </strong>
+                        {/* <input type="text" value={`${session?.person_first_name} ${session?.person_last_name}`} readOnly /> */}
+                    </p>
+                    <input type="text" value={`${session?.person_first_name} ${session?.person_last_name}`} readOnly />
+                </div>
+
+                <div className="grid grid-cols-[100px_1fr] items-center gap-3">
+                    <p className="text-white mb-2">
+                        <strong>Line: </strong>
+                        {/* <input type="dropdown" readOnly={false} onChange={(e) => setLine(e.target.value)}  /> */}
+                    </p>
                     <select
                         value={line}
                         onChange={(e) => setLine(e.target.value)}
                         className="mt-1 bg-gray-700 rounded-lg shadow-lg border border-gray-600 py-1">
                         <option value="">Select a line</option>
 
-                        {productionLines.map((lineName, index) => (
-
-                            <option key={index} value={lineName}>
-                                {lineName}
-                            </option>
-                        ))}
+                        {productionLines
+                            .filter((lineName) => lineName !== "All Lines")
+                            .map((lineName, index) => (
+                                <option key={index} value={lineName}>
+                                    {lineName}
+                                </option>
+                            ))}
                     </select>
+                </div>
 
-                </p>
-                <p className="text-white mb-2">
-                    <strong>Date: </strong>
+                <div className="grid grid-cols-[100px_1fr] items-center gap-3">
+                    <p className="text-white mb-2">
+                        <strong>Date: </strong>
+                    </p>
                     <input type="text" value={date} readOnly={false} onChange={(e) => setDate(e.target.value)} />
+                </div>
 
-                </p>
-                <p className="text-white mb-2">
-                    <strong>Login: </strong>
+                <div className="grid grid-cols-[100px_1fr] items-center gap-3">
+                    <p className="text-white mb-2">
+                        <strong>Login: </strong>
+                    </p>
                     <input type="text" value={loginTime} readOnly={false} onChange={(e) => setLoginTime(e.target.value)} />
-                </p>
-                <p className="text-white mb-2">
-                    <strong>Logout: </strong>
+                </div>
+
+                <div className="grid grid-cols-[100px_1fr] items-center gap-3">
+                    <p className="text-white mb-2">
+                        <strong>Logout: </strong>
+                    </p>
                     <input type="text" value={logoutTime} readOnly={false} onChange={(e) => setLogoutTime(e.target.value)} />
-                </p>
+                </div>
 
                 <div className="text-white mb-2  flex justify-between">
-                    <button
-                        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-                        onClick={async () => {
-                            const updatedSession: EmployeeSession = {
-                                ...session,
-                                line_name: line,
-                                login_timestamp: new Date(
-                                    `${date} ${loginTime}`
-                                ).toISOString(),
-                                logout_timestamp:
-                                    logoutTime && logoutTime !== "-"
-                                        ? new Date(`${date} ${logoutTime}`).toISOString()
-                                        : null,
-                            };
-
-                            await save(updatedSession);
-
-                            onClose();
-                        }}
-                    >
-                        Save
-                    </button>
-
                     <button
                         className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
                         onClick={
@@ -166,12 +161,38 @@ const Popup: React.FC<PopupProps> = ({ isOpen, session, onClose, save, deleteSes
                         Delete
                     </button>
 
-                    <button
-                        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-                        onClick={onClose}
-                    >
-                        Close
-                    </button>
+
+                    <div className="flex gap-3">
+                        <button
+                            className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+                            onClick={onClose}
+                        >
+                            Close
+                        </button>
+
+                        <button
+                            className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+                            onClick={async () => {
+                                const updatedSession: EmployeeSession = {
+                                    ...session,
+                                    line_name: line,
+                                    login_timestamp: new Date(
+                                        `${date} ${loginTime}`
+                                    ).toISOString(),
+                                    logout_timestamp:
+                                        logoutTime && logoutTime !== "-"
+                                            ? new Date(`${date} ${logoutTime}`).toISOString()
+                                            : null,
+                                };
+
+                                await save(updatedSession);
+
+                                onClose();
+                            }}
+                        >
+                            Save
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
